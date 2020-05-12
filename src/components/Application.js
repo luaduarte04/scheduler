27643a4fc1c791ba4e2c31f606d8ab6b038c499e
Luana Duarte <lua.duarte04@gmail.com>
2020-05-12 15:10:35 -0400

    added appointment info into app

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,55 +1,120 @@
-import React, { useState } from "react";
+import React, { useState, Fragment, useEffect } from "react";
+import axios from 'axios';
 
 import "components/Application.scss";
 
+import { getAppointmentsForDay } from "helpers/selectors";
+
 import DayList from "components/DayList";
 
+import Appointment from "components/Appointment";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
-const days = [
+
+const appointments = [
   {
     id: 1,
-    name: "Monday",
-    spots: 2,
+    time: "12pm",
   },
   {
     id: 2,
-    name: "Tuesday",
-    spots: 5,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
   },
   {
     id: 3,
-    name: "Wednesday",
-    spots: 0,
+    time: "2pm",
   },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Luana Duarte",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Paul Carter",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  }
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
   
+  const setDay = (day) => setState({...state, day: day});
+
+  useEffect(() => {
+    axios({
+      method: "GET",
+      url: "http://localhost:8001/api/days"
+    })
+      .then(result => {
+        setState({...state, days: result.data});
+      })
+  }, []);
+
   return (
     <main className="layout">
       <section className="sidebar">
         <img
-          className="sidebar--centered"
-          src="images/logo.png"
-          alt="Interview Scheduler"
+        className="sidebar--centered"
+        src="images/logo.png"
+        alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
-          />
+            />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
-        />
+          />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        { appointments.map(appointment => {
+          return (
+            <Appointment key={appointment.id} {...appointment} />
+            );
+          })
+        }
+        <Appointment id="last" time="5pm" />
       </section>
     </main>
   );
