7bb4d4ce2afd36a4bc455aa3720504cff0b21f5b
Luana Duarte <lua.duarte04@gmail.com>
2020-05-12 15:00:07 -0400

    created appointment form

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,10 +5,10 @@ import "components/Appointment/styles.scss";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
 import Show from "components/Appointment/Show";
-import Confirm from "components/Appointment/Confirm";
-import Status from "components/Appointment/Status";
-import Error from "components/Appointment/Error";
-import Form from "components/Appointment/Form";
+// import Confirm from "components/Appointment/Confirm";
+// import Status from "components/Appointment/Status";
+// import Error from "components/Appointment/Error";
+// import Form from "components/Appointment/Form";
 
 
 export default function Appointment(props) {
--- stories/index.js
+++ stories/index.js
@@ -16,11 +16,11 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
-// import Show from "components/Appointment/Show";
-// import Confirm from "components/Appointment/Confirm";
-// import Status from "components/Appointment/Status";
-// import Error from "components/Appointment/Error";
-// import Form from "components/Appointment/Form";
+import Show from "components/Appointment/Show";
+import Confirm from "components/Appointment/Confirm";
+import Status from "components/Appointment/Status";
+import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 // STORY OF Button
