c93e472668375603cf1ba9d9138971491039b54e
Luana Duarte <lua.duarte04@gmail.com>
2020-05-20 16:48:15 -0400

    cypress installed

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,17 @@
+describe("Appointments", () => {
+  cy.request("GET", "/api/debug/reset");
+
+  it("should book an interview", () => {
+    cy.visit("/");
+    cy.contains("Monday");
+
+    cy.get("[alt=Add]")
+    .first()
+    .click();
+
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones");
+    cy.get("[alt='Sylvia Palmer']").click();
+
+    cy.contains("Save").click();
+  });
+});
